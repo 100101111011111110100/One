4a29487a82e0ade83fa09b63806d9777793bf642
Kotayka <arturzamaletdinov@gmail.com>
2024-03-30 11:22:38 +0300

    Add polish 1.0

--- simpletron/main.c
+++ simpletron/main.c
@@ -1,14 +1,8 @@
 #include"main.h"
 int main(){
 	node memory[MEM];
-	//print();
-	//if(command_init(memory,choise())){
 	if(command_read(memory)){
       if(brain(memory)==0) printf("n/a\n");
-		/*for(int i=0;i<MEM;i++){
-			printf("%d | %d\n",memory[i].num.i,memory[i].type);
-		}*/
 	}else	printf("n/a\n");
-	//dumb_print(memory);
    return 0;
 }
